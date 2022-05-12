b0c26e5088c88e3230e94f6e4592d3c6fee526aa
AhlamAA <98388927+AhlamAA@users.noreply.github.com>
2022-05-12 13:06:23 +0300

    add some new empty files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import Home from './pages/Home';
+import Recipes from './pages/Recipes';
+import OneRecipe from './pages/OneRecipe';
 
 
 const theme = createTheme({
@@ -16,6 +18,8 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
         <Home/>
+        {/* <Recipes /> */}
+        {/* <OneRecipe /> */}
     </ThemeProvider>
   )
 }
---
+++ src/pages/OneRecipe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../componants/Navbar'
+import Footer from '../componants/Footer';
+
+export default function OneRecipe() {
+  return (
+      <>
+        <Navbar/>
+        
+        <Footer />
+      </>
+  )
+}
